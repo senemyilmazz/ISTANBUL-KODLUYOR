637e8c0ac33cd1c592a32a57e3e44da069b09f5f
Senem <132691238+senemyilmazz@users.noreply.github.com>
2023-10-26 19:39:57 +0300

    Delete oopWithNLayeredApp directory

--- oopWithNLayeredApp/src/oopWithNLayeredApp/core/logging/FileLogger.java
+++
@@ -1,9 +0,0 @@
-package oopWithNLayeredApp.core.logging;
-
-public class FileLogger implements Logger {
-
-	@Override //zorunlu değil interface veya abstracttan geldiğini belirtir.
-	public void log(String data) {
-		System.out.println("Dosyaya loglandı :" + data);
-	}
-}
--- oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package oopWithNLayeredApp.dataAccess;
-
-
-import oopWithNLayeredApp.entities.Product;
-
-public interface ProductDao {
-	void add(Product product);
-	//Jdbc ve hibernate in içindeki metotların buna uygun olması lazım.
-}
